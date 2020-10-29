fca5cb3257ac5ce2afe178b8a3500207badd760a
Masakra <danil.b.shein@gmail.com>
2020-10-29 12:24:04 +0300

    Added new line of code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#/bin/python3
+#!/bin/python3
+
+import datetime as dt
 
 def main():
     print("Hello World!")
     print("It's alive!")
+    print(dt.datetime.now())
 
 if __name__ == '__main__':
     main()
