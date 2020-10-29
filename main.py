133d5340bda06a5c4b1a859725ee96c30cafaa88
DanilShein <73377831+DanilShein@users.noreply.github.com>
2020-10-29 12:29:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime as dt
 def main():
     print("Hello World!")
     print("It's alive!")
-    print(dt.datetime.now())
+    print("Current time and date is: ", dt.datetime.now())
 
 if __name__ == '__main__':
     main()
